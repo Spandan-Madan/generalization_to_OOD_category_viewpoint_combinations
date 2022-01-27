e515aef9cff8406c311036a6c313ebda2d42ad01
spandan_madan@g.harvard.edu <smadan@polestar.ib.cluster>
2022-01-27 18:33:30 -0500

    adding mnist rotation download scripts'

---
+++ utils/download_mnist_rotation.sh
@@ -0,0 +1,8 @@
+cd ../
+mkdir -p data/
+cd data/
+mkdir -p mnist_rotation
+cd mnist_rotation
+
+wget https://www.dropbox.com/s/wdws3b3fjo190sk/self_generated.tar.gz?dl=0
+tar -zxf self_generated.tar.gz
