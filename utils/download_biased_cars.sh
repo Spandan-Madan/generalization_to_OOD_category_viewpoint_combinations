dbf4fcdba9d13fb0c00c62e7c2ccdbf2a9ab5a3b
spandan_madan@g.harvard.edu <smadan@polestar.ib.cluster>
2022-01-27 18:22:44 -0500

    adding utils to download files

---
+++ utils/download_biased_cars.sh
@@ -0,0 +1,36 @@
+echo 'Building directory structure...'
+cd ../
+mkdir -p data/
+cd data/
+mkdir -p biased_cars
+cd biased_cars
+
+echo 'Directories done. Downloading data...'
+wget https://dataverse.harvard.edu/api/access/datafile/5793795
+mv 5793795 att_dict_simplified.p
+
+wget https://dataverse.harvard.edu/api/access/datafile/5347218
+mv 5347218 biased_cars_1.zip
+
+wget https://dataverse.harvard.edu/api/access/datafile/5347225
+mv 5347225 biased_cars_2.zip
+
+wget https://dataverse.harvard.edu/api/access/datafile/5347224
+mv 5347224 biased_cars_3.zip
+
+wget https://dataverse.harvard.edu/api/access/datafile/5347223
+mv 5347223 biased_cars_4.zip
+
+echo 'Downloaded, unzipping files...'
+unzip biased_cars_1.zip
+unzip biased_cars_2.zip
+unzip biased_cars_3.zip
+unzip biased_cars_4.zip
+
+echo 'moving files to correct directories'
+mv biased_cars_1/* ./ 
+mv biased_cars_2/* ./
+mv biased_cars_3/* ./
+mv biased_cars_4/* ./
+
+echo 'All done!'
