ba63be9cfb1ae8aa9a4a04cfc6ae102b50fa1adf
spandan_madan@g.harvard.edu <smadan@polestar.ib.cluster>
2022-01-30 00:52:12 -0500

    non verbose untar

--- utils/download_mnist_rotation.sh
+++ utils/download_mnist_rotation.sh
@@ -5,5 +5,5 @@ mkdir -p mnist_rotation
 cd mnist_rotation
 
 wget https://www.dropbox.com/s/wdws3b3fjo190sk/self_generated.tar.gz?dl=0 -O self_generated.tar.gz
-tar -zxvf self_generated.tar.gz
+tar -zxf self_generated.tar.gz
 rm self_generated.tar.gz
